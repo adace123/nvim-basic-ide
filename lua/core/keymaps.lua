383b99f9402968a95f7d0286622131ec55683eee
aaron.feigenbaum <aaron@enervee.com>
2022-12-28 02:04:51 -0800

    feat: add line move keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -64,6 +64,10 @@ keymap("i", "kj", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Move lines
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+
 -- Plugins --
 
 -- NvimTree
