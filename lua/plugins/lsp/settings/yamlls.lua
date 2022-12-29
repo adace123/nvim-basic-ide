aaaac877161fe10c61dbad6d0d43b66d8faae639
aaron.feigenbaum <aaron@enervee.com>
2022-12-28 21:26:59 -0800

    feat: add more yaml schemas

--- lua/plugins/lsp/settings/yamlls.lua
+++ lua/plugins/lsp/settings/yamlls.lua
@@ -3,8 +3,13 @@ return {
 		yaml = {
 			schemas = {
 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-				["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
-				["https://json.schemastore.org/chart.json"] = "Chart.yaml",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+				["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+				["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
 			},
 			format = {
 				enable = true,
