fb0605f513444da7dfadecbc6aed68125eeaecb0
aaron.feigenbaum <aaron@enervee.com>
2022-12-28 23:14:12 -0800

    fix: restore gitsigns config

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -151,7 +151,12 @@ local plugins = {
 	},
 
 	-- Git
-	{ "lewis6991/gitsigns.nvim", lazy = false },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 
 	-- DAP
 	{
