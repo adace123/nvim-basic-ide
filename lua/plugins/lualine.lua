ca0076da21844c29a134e86e6d9b9a5f590f2df1
aaron.feigenbaum <aaron@enervee.com>
2023-01-03 23:02:00 -0800

    change lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,11 +3,13 @@ if not status_ok then
 	return
 end
 
+local theme = require("lualine.themes.palenight")
+
 lualine.setup({
 	options = {
 		globalstatus = true,
 		icons_enabled = true,
-		theme = "auto",
+		theme = theme,
 		disabled_filetypes = { "alpha", "dashboard" },
 		always_divide_middle = true,
 	},
