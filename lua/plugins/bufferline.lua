6efe7e9aa1149814161fc98e380456ce7c0f65a7
aaron.feigenbaum <aaron@enervee.com>
2023-01-12 22:02:27 -0800

    change bufferline sep style

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,7 +8,7 @@ bufferline.setup({
 		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-		separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+		separator_style = "padded_slant", -- | "thick" | "thin" | { 'any', 'any' },
 		diagnostics = "nvim_lsp",
 		diagnostics_indicator = function(_, _, diagnostics_dict, _)
 			local s = ""
