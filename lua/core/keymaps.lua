08e7780b56c5a2b40716b42c9cdb683f1a8019c7
aaron.feigenbaum <aaron@enervee.com>
2023-01-19 23:51:30 -0800

    add more buffer nav keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -34,6 +34,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<TAB>", ":bnext<CR>", opts)
+keymap("n", "<S-TAB>", ":bprevious<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
