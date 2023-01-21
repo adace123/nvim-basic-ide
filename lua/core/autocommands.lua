fb9fdf5c58a23519f9ed16af93ca5cd816d409d7
aaron.feigenbaum <aaron@enervee.com>
2023-01-21 14:33:12 -0800

    add spellcheck autocommand

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -79,3 +79,16 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		vim.opt.formatoptions:remove({ "c", "r", "o" })
 	end,
 })
+
+-- Enable spell checking for certain file types
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,de"
+    end,
+  }
+)
